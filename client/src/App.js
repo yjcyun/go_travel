fa4d06f6515d29cc516465125e48b0bac6e9d198
Christina Yujeong Yun <yjc.yun@gmail.com>
2020-09-08 15:31:10 -0400

    Remove pug files and create cra inside client folder

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>hi</div>
+  
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose
   })
   .then(() => console.log('DB connection successful'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
